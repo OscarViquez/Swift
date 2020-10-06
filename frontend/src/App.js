07e20ec8674b6c042040a6ea46fd85c5d1b9152b
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2020-10-06 15:57:02 -0400

    added landing page and added several pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1> Welcome to Pro Shop!</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          {/* the exact attr means that it should EXACTly
+          say "http://localhost:3000/"
+          <--- The slash needs to be included in order to go on to the home page --->
+          */}
+          <Route path='/product/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
